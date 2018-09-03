5d1df25fb905f05faea48b2185df6caeb60f881b
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2018-09-03 14:24:54 -0500

    Adding advanced indexing (based on integer arrays) for 1d targets

--- phylanx/util/slicing_helpers.hpp
+++ phylanx/util/slicing_helpers.hpp
@@ -29,12 +29,17 @@ namespace phylanx { namespace util { namespace slicing_helpers
 
     ///////////////////////////////////////////////////////////////////////////
     // generate a list of indices to extract from a given vector
-    PHYLANX_EXPORT std::vector<std::int64_t> create_list_slice(
+    PHYLANX_EXPORT std::vector<std::size_t> create_list_slice(
         std::int64_t start, std::int64_t stop, std::int64_t step);
 
     ///////////////////////////////////////////////////////////////////////////
     // extract a list of indices that correspond to the given slicing parameters
     PHYLANX_EXPORT ir::slicing_indices extract_slicing(
+        execution_tree::primitive_argument_type const& arg,
+        std::size_t arg_size, std::string const& name = "",
+        std::string const& codename = "<unknown>");
+
+    PHYLANX_EXPORT std::size_t slicing_size(
         execution_tree::primitive_argument_type const& arg,
         std::size_t arg_size, std::string const& name = "",
         std::string const& codename = "<unknown>");
